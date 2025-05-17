be8dff83dc269f17db57ba8e02dab4f02dd6faa9
TheWestWings <zhengzilong275500@outlook.com>
2025-05-17 14:09:13 +0800

    feat: add Order class with properties and getter/setter methods

---
+++ src/main/java/com/mumuwest/mumumike/pojo/Order.java
@@ -0,0 +1,61 @@
+package com.mumuwest.mumumike.pojo;
+
+import java.util.Date;
+
+public class Order {
+    private Integer id;
+    private Integer userId;
+    private String productId;
+    private Integer count;
+    private Integer status;
+    private Date createTime;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
