661a3d4e963934b24bffb22e84766b94e86f9aee
TheWestWings <zhengzilong275500@outlook.com>
2025-05-17 15:50:42 +0800

    feat: add Product class with properties and getter/setter methods

---
+++ src/main/java/com/mumuwest/mumumike/pojo/Product.java
@@ -0,0 +1,49 @@
+package com.mumuwest.mumumike.pojo;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer price;
+    private String picture;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
