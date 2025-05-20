1769a2d05a949588df0e2d7258f5421c3a2f58ad
TheWestWings <zhengzilong275500@outlook.com>
2025-05-21 02:24:24 +0800

    feat: enhance UI with new button styles and layout adjustments

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-
     <router-view></router-view>
-
-  
 </template>
 
 <script>
@@ -12,5 +9,34 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.5;
+  color: #333;
+  background-color: #fff;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul, ol {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  vertical-align: middle;
+}
 </style>
