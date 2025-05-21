8d34d86f532a8efa02eba261c91c9803e12133c4
West. <zhengzilong275500@outlook.com>
2025-05-21 21:30:14 +0800

    feat: add car module to manage car list state with local storage integration

---
+++ web/src/store/car.js
@@ -0,0 +1,25 @@
+const carModule = {
+
+    namespaced: true,
+
+    state: {
+        carList: localStorage.getItem('carList') ? JSON.parse(localStorage.getItem('cart')) : [],
+    },
+
+    getters: {
+
+    },
+
+    mutations: {
+        setCarList(state, carList) {
+            state.carList = carList;
+            localStorage.setItem('carList', JSON.stringify(carList));
+        },
+        clearCarList(state) {
+            state.carList = [];
+            localStorage.removeItem('carList');
+        },
+    },
+};
+
+export default carModule;
