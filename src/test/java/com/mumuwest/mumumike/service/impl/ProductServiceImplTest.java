17796b512faafcfe78ce4253219d9ddefaec103e
郑子龙 <15025745+TheWestWings@user.noreply.gitee.com>
2026-05-06 14:11:12 +0800

    成功

--- src/test/java/com/mumuwest/mumumike/service/impl/ProductServiceImplTest.java
+++ src/test/java/com/mumuwest/mumumike/service/impl/ProductServiceImplTest.java
@@ -13,7 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
-// 你好
+// 成功
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
